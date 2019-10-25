3ba1a62ae1648a9c6c5a67d5ed6faf2ecb884f61
Robert K. Bell <robert.k.bell@gmail.com>
2019-10-25 18:15:09 +1100

    feat: implement using Parsers

--- src/NucleotideCount.elm
+++ src/NucleotideCount.elm
@@ -1,5 +1,14 @@
 module NucleotideCount exposing (nucleotideCounts)
 
+import Parser exposing ((|=), Parser, map, symbol)
+
+
+type Nucleotide
+    = Adenine
+    | Cytosine
+    | Guanine
+    | Thymine
+
 
 type alias NucleotideCounts =
     { a : Int
@@ -11,4 +20,55 @@ type alias NucleotideCounts =
 
 nucleotideCounts : String -> NucleotideCounts
 nucleotideCounts sequence =
-    Debug.todo "Please implement this function"
+    case sequence |> Parser.run decodeDNA of
+        Ok dna ->
+            dna |> List.foldl count allZeros
+
+        Err _ ->
+            allZeros
+
+
+allZeros =
+    { a = 0
+    , c = 0
+    , g = 0
+    , t = 0
+    }
+
+
+decodeDNA : Parser (List Nucleotide)
+decodeDNA =
+    Parser.sequence
+        { start = ""
+        , separator = ""
+        , end = ""
+        , spaces = Parser.spaces
+        , item = decodeNucleotide
+        , trailing = Parser.Optional
+        }
+
+
+decodeNucleotide : Parser Nucleotide
+decodeNucleotide =
+    Parser.oneOf
+        [ symbol "A" |> map (\_ -> Adenine)
+        , symbol "C" |> map (\_ -> Cytosine)
+        , symbol "G" |> map (\_ -> Guanine)
+        , symbol "T" |> map (\_ -> Thymine)
+        ]
+
+
+count : Nucleotide -> NucleotideCounts -> NucleotideCounts
+count nucleotide dna =
+    case nucleotide of
+        Adenine ->
+            { dna | a = dna.a + 1 }
+
+        Cytosine ->
+            { dna | c = dna.c + 1 }
+
+        Guanine ->
+            { dna | g = dna.g + 1 }
+
+        Thymine ->
+            { dna | t = dna.t + 1 }
